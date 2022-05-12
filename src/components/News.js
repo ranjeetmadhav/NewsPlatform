82969958b04104397d911129fd09518d69c6c231
rahuljacker27 <rkkumar421@gmail.com>
2022-05-12 17:22:45 +0530

    before making functions based components

--- src/components/News.js
+++ src/components/News.js
@@ -34,7 +34,7 @@ export class News extends Component {
     }
 
     async updateNews(){
-      // console.log("i am in uN");
+      console.log("i am in uN");
       this.props.setProgress(10);
       const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       this.setState({loading: true});
@@ -51,7 +51,7 @@ export class News extends Component {
 
     async componentDidMount(){
       this.updateNews();
-      // console.log("i am in cdm");
+      console.log("i am in cdm");
   
     }
 
@@ -70,7 +70,7 @@ export class News extends Component {
       
       this.setState({page: this.state.page+1});
         
-        const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+        const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
         
         let data = await fetch(url);
         let parsedData =await data.json();
